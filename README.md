6a2e66242f5e5ac91b74c2f93ac39d8d6524f12b
José Jácome <josejacomeb@gmail.com>
2022-04-12 21:54:05 -0500

    Adding first files of the project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Practice_Docker_Flask_MediaPlayer
 Program to practice the Docker platform, along with the Flask, OpenCV and Mosquitto libraries for transmitting image data to a webserver 
+
+Images generated with Docker's BuildKit, example of generation
+
+```commandline
+DOCKER_BUILDKIT=1 docker build --tag media_player .
+```
---
+++ media_player/code/media_player.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import logging
+import os
+
+import cv2
+"""
+    Class to 
+"""
+class MediaPlayer:
+    """
+
+    """
+    def __init__(self, source):
+        self.source = source
+
+    def setup(self):
+        pass
+
+    def play(self):
+        pass
+
+    def pause(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def backward_once(self):
+        pass
